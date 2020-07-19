47de147c89206479bb9452e1067edd26a836b74d
Prava-Smrite <pravasmrite97@gmail.com>
2020-07-19 21:24:47 +0600

    project complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//login button development
+//login button event create
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function () {
     const loginArea = document.getElementById("login-area");
@@ -6,18 +6,30 @@ loginBtn.addEventListener("click", function () {
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
 })
-//deposit button development
+//deposit button event create
 const dipositBtn = document.getElementById("diposit-btn");
 dipositBtn.addEventListener("click", function () {
-    const inputAmount = document.getElementById("input-amount").value;
-    const currentInputAmount = parseFloat(inputAmount);
-    const depositAmount = document.getElementById("deposit-amount").innerText;
-    const currentdepositAmount = parseFloat(depositAmount);
-    const totalAmount = currentInputAmount + currentdepositAmount;
-    document.getElementById("deposit-amount").innerText = totalAmount;
+    const currentInputAmount = getInputNumber("input-amount");
+    updateSpanText("deposit-amount", currentInputAmount)
+    updateSpanText("balance-amount", currentInputAmount)
     document.getElementById("input-amount").value = "";
-    const balanceAmount = document.getElementById("balance-amount").innerText;
-    const currentbalanceAmount = parseFloat(balanceAmount);
-    const totalBalance = currentbalanceAmount + currentInputAmount;
-    document.getElementById("balance-amount").innerText = totalBalance;
 })
+//withdraw button event create
+const withdrawBtn = document.getElementById("withdraw-btn");
+withdrawBtn.addEventListener("click", function () {
+    const currentWithdrawAmount = getInputNumber("withdraw-amount");
+    updateSpanText("withdraw-input", currentWithdrawAmount);
+    updateSpanText("balance-amount", -1 * currentWithdrawAmount);
+    document.getElementById("withdraw-amount").value = "";
+})
+function getInputNumber(id) {
+    const Amount = document.getElementById(id).value;
+    const currentAmount = parseFloat(Amount);
+    return currentAmount;
+}
+function updateSpanText(id, currentInputAmount) {
+    const balanceAmount = document.getElementById(id).innerText;
+    const currentbalanceAmount = parseFloat(balanceAmount);
+    const totalBalance = currentInputAmount + currentbalanceAmount;
+    document.getElementById(id).innerText = totalBalance;
+}
